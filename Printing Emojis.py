14d33fb0ecd21d837759df1215561bdc4f1802e3
Mohit1108 <73032070+Mohit1108@users.noreply.github.com>
2021-01-20 20:15:47 +0530

    Add files via upload

---
+++ Printing Emojis.py
@@ -0,0 +1,11 @@
+print("\U0001F602") # For HAHA 😂
+print("\U0001F92A") # For one Eye Blink 🤪
+print("\U0001F92B") # For SHH 🤫
+print("\U0001F605") # For Laugh 😅
+print("\U0001F60D") # For Loving hearts from Eyes 😍
+
+
+
+
+
+
